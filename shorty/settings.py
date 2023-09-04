92cd906528d0d03675953f298f856b4da14636d5
Anasdevs <anassaifi1108@gmail.com>
2023-09-04 12:30:50 +0530

    added Some more styling

--- shorty/settings.py
+++ shorty/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 
 # Define the directory where static files are located
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
